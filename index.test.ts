abb08bbc9bf8c814672a090f7a25d8700678e5d5
Stefan Terdell <stefan.terdell@live.com>
2021-02-27 22:58:54 +0100

    ✅ Tests for native enums

--- index.test.ts
+++ index.test.ts
@@ -335,6 +335,56 @@ describe('Unions', () => {
   });
 });
 
+describe('Native enums', () => {
+  it('should be possible to convert a basic native number enum', () => {
+    enum MyEnum {
+      val1,
+      val2,
+      val3,
+    }
+
+    const parsedSchema = toJsonSchema(z.nativeEnum(MyEnum));
+    const jsonSchema: JSONSchema7 = {
+      $schema: 'http://json-schema.org/draft-07/schema#',
+      type: 'number',
+      enum: [0, 1, 2],
+    };
+    expect(parsedSchema).toStrictEqual(jsonSchema);
+  });
+
+  it('should be possible to convert a native string enum', () => {
+    enum MyEnum {
+      val1 = 'a',
+      val2 = 'b',
+      val3 = 'c',
+    }
+
+    const parsedSchema = toJsonSchema(z.nativeEnum(MyEnum));
+    const jsonSchema: JSONSchema7 = {
+      $schema: 'http://json-schema.org/draft-07/schema#',
+      type: 'string',
+      enum: ['a', 'b', 'c'],
+    };
+    expect(parsedSchema).toStrictEqual(jsonSchema);
+  });
+
+  it('should be possible to convert a mixed value native enum', () => {
+    enum MyEnum {
+      val1 = 'a',
+      val2 = 1,
+      val3 = 'c',
+    }
+
+    const parsedSchema = toJsonSchema(z.nativeEnum(MyEnum));
+    const jsonSchema: JSONSchema7 = {
+      $schema: 'http://json-schema.org/draft-07/schema#',
+      type: ['string', 'number'],
+      enum: ['a', 1, 'c'],
+    };
+    expect(parsedSchema).toStrictEqual(jsonSchema);
+  });
+});
+
 describe('Output validation', () => {
   it('String errors should match between Z and Ajv', () => {
     const zodSchema = z.string().min(2).max(4);
