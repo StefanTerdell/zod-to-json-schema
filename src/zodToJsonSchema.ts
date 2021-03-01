43f8bcde42fae14d107d00ed11d627f971c330a1
Stefan Terdell <stefan.terdell@live.com>
2021-03-01 23:57:16 +0100

    🏷️ Conditional root return type

--- src/zodToJsonSchema.ts
+++ src/zodToJsonSchema.ts
@@ -1,29 +1,37 @@
 import { Schema } from 'zod';
 import { JsonSchema7Type, parseDef } from './parseDef';
 
-export type JsonSchema7 = {
+/**
+ * @param schema The Zod schema to be converted
+ */
+function zodToJsonSchema(
+  schema: Schema<any>
+): {
   $schema: 'http://json-schema.org/draft-07/schema#';
-} & (
-  | {
-      $ref: string;
-      definitions: Record<string, JsonSchema7Type>;
-    }
-  | JsonSchema7Type
-);
-
+} & JsonSchema7Type;
 /**
  * @param schema The Zod schema to be converted
  * @param name The (optional) name of the schema. If a name is passed, the schema will be put in 'definitions' and referenced from the root.
  */
-export function zodToJsonSchema(schema: Schema<any>, name?: string): JsonSchema7 {
-  return name
+function zodToJsonSchema<T extends string>(
+  schema: Schema<any>,
+  name: T
+): {
+  $schema: 'http://json-schema.org/draft-07/schema#';
+  $ref: string;
+  definitions: Record<T, JsonSchema7Type>;
+};
+function zodToJsonSchema(schema: Schema<any>, name?: string) {
+  return name === undefined
     ? {
         $schema: 'http://json-schema.org/draft-07/schema#',
-        $ref: `#/definitions/${name}`,
-        definitions: { [name]: parseDef(schema._def, ['definitions', name], []) || {} },
+        ...parseDef(schema._def, [], []),
       }
     : {
         $schema: 'http://json-schema.org/draft-07/schema#',
-        ...parseDef(schema._def, [], []),
+        $ref: `#/definitions/${name}`,
+        definitions: { [name]: parseDef(schema._def, ['definitions', name], []) || {} },
       };
 }
+
+export { zodToJsonSchema };
