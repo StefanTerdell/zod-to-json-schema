f33ae25bb72a45687f57995a0e1fb015ca1de1ff
Hammad Asif <hammad@nitroxis.com>
2021-06-15 15:32:51 +0500

    remove: extraneous file

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod"
-import { zodToJsonSchema } from "./src/zodToJsonSchema"
-
-const a = zodToJsonSchema(z.object({
-  hammad: z.string(),
-  age: z.number().min(18).max(45)
-}))
-
-console.log(JSON.stringify(a, null, 2))
