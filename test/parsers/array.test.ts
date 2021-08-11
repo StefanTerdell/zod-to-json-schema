4cc0a7930cd6e12e31467346efe7bcac180713ce
Stefan Terdell <stefan.terdell@live.com>
2021-08-11 17:22:27 +0200

    ✅ Fix broken calls in array tests

--- test/parsers/array.test.ts
+++ test/parsers/array.test.ts
@@ -1,54 +1,52 @@
-import { JSONSchema7Type } from 'json-schema';
-import { z } from 'zod';
-import { parseArrayDef } from '../../src/parsers/array';
+import { JSONSchema7Type } from "json-schema";
+import { z } from "zod";
+import { parseArrayDef } from "../../src/parsers/array";
 
-describe('Arrays and array validations', () => {
-  it('should be possible to describe a simple array', () => {
-    const parsedSchema = parseArrayDef(z.array(z.any())._def, [], [], false);
+describe("Arrays and array validations", () => {
+  it("should be possible to describe a simple array", () => {
+    const parsedSchema = parseArrayDef(z.array(z.any())._def, [], []);
     const jsonSchema: JSONSchema7Type = {
-      type: 'array',
+      type: "array",
       items: {},
     };
     expect(parsedSchema).toStrictEqual(jsonSchema);
   });
-  it('should be possible to describe a string array with a minimum and maximum length', () => {
+  it("should be possible to describe a string array with a minimum and maximum length", () => {
     const parsedSchema = parseArrayDef(
       z.array(z.string()).min(2).max(4)._def,
       [],
-      [],
-      false
+      []
     );
     const jsonSchema: JSONSchema7Type = {
-      type: 'array',
+      type: "array",
       items: {
-        type: 'string',
+        type: "string",
       },
       minItems: 2,
       maxItems: 4,
     };
     expect(parsedSchema).toStrictEqual(jsonSchema);
   });
-  it('should be possible to describe a string array with a minimum length of 1 by using nonempty', () => {
+  it("should be possible to describe a string array with a minimum length of 1 by using nonempty", () => {
     const parsedSchema = parseArrayDef(
       z.array(z.any()).nonempty()._def,
       [],
-      [],
-      true
+      []
     );
     const jsonSchema: JSONSchema7Type = {
-      type: 'array',
+      type: "array",
       items: {},
       minItems: 1,
     };
     expect(parsedSchema).toStrictEqual(jsonSchema);
   });
-  
-  it ('should be possible do properly reference array items', () => {
-    const willHaveBeenSeen = z.object({hello: z.string()})
-    const unionSchema = z.union([willHaveBeenSeen, willHaveBeenSeen])
-    const arraySchema = z.array(unionSchema)
-    const jsonSchema = parseArrayDef(arraySchema._def, [], [], false)
+
+  it("should be possible do properly reference array items", () => {
+    const willHaveBeenSeen = z.object({ hello: z.string() });
+    const unionSchema = z.union([willHaveBeenSeen, willHaveBeenSeen]);
+    const arraySchema = z.array(unionSchema);
+    const jsonSchema = parseArrayDef(arraySchema._def, [], []);
     //TODO: Remove 'any'-cast when json schema type package supports it. 'anyOf' in 'items' should be completely according to spec though.
-    expect((jsonSchema.items as any).anyOf[1].$ref).toEqual('#/items/anyOf/0')
-  })
+    expect((jsonSchema.items as any).anyOf[1].$ref).toEqual("#/items/anyOf/0");
+  });
 });
