cb651055474d724e8e9af3e6e1773df910b54df1
Stefan Terdell <stefan.terdell@live.com>
2021-08-11 17:22:05 +0200

    ✨ Simplified intersections further

--- src/parsers/intersection.ts
+++ src/parsers/intersection.ts
@@ -1,32 +1,18 @@
-import { ZodIntersectionDef, ZodParsedType } from "zod";
+import { ZodIntersectionDef } from "zod";
 import { JsonSchema7Type, parseDef, Visited } from "../parseDef";
-import { parseObjectDef } from "./object";
 
 export function parseIntersectionDef(
   def: ZodIntersectionDef,
   path: string[],
   visited: Visited
 ): JsonSchema7Type | undefined {
-  const rightDef = def.right._def;
-  const leftDef = def.left._def;
-  if (
-    rightDef.t === ZodParsedType.object &&
-    leftDef.t === ZodParsedType.object
-  ) {
-    const right = parseObjectDef(rightDef, path, visited);
-    const left = parseObjectDef(leftDef, path, visited);
-    return {
-      type: "object",
-      properties: { ...left.properties, ...right.properties },
-      required: [
-        ...(left.required || []).filter(
-          (x) => !Object.keys(right.properties).includes(x)
-        ),
-        ...(right.required || []),
-      ],
-    };
-  }
-  return {
-    allOf: [parseDef(leftDef, path, visited), parseDef(rightDef, path, visited)],
-  };
+  const allOf = [
+    parseDef(def.left._def, path, visited),
+    parseDef(def.right._def, path, visited),
+  ].filter(Boolean);
+  return allOf.length === 2
+    ? { allOf }
+    : allOf.length === 1
+    ? allOf[0]
+    : undefined;
 }
