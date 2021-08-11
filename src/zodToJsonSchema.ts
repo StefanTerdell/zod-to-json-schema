da0791e2ab88df4831603d14a40939122ddcb299
Stefan Terdell <stefan.terdell@live.com>
2021-08-11 08:06:53 +0200

    ✨ Fixed broken external typings

--- src/zodToJsonSchema.ts
+++ src/zodToJsonSchema.ts
@@ -1,27 +1,27 @@
 import { ZodSchema } from "zod";
 import { JsonSchema7Type, parseDef } from "./parseDef";
 
+type JsonSchema7Definitions = { [key: string]: JsonSchema7Type };
+
 /**
  * @param schema The Zod schema to be converted
  */
 function zodToJsonSchema(
   schema: ZodSchema<any>
-):
-  | ({
-      $schema: "http://json-schema.org/draft-07/schema#";
-    } & JsonSchema7Type)
-  | unknown;
+): {
+  $schema: "http://json-schema.org/draft-07/schema#";
+} & JsonSchema7Type;
 /**
  * @param schema The Zod schema to be converted
  * @param name The (optional) name of the schema. If a name is passed, the schema will be put in 'definitions' and referenced from the root.
  */
-function zodToJsonSchema<T extends string>(
+function zodToJsonSchema<Name extends string>(
   schema: ZodSchema<any>,
-  name: T
+  name: Name
 ): {
   $schema: "http://json-schema.org/draft-07/schema#";
-  $ref: string;
-  definitions: Record<T, JsonSchema7Type>;
+  $ref: `#/definitions/${Name}`;
+  definitions: JsonSchema7Definitions;
 };
 function zodToJsonSchema(schema: ZodSchema<any>, name?: string) {
   return name === undefined
