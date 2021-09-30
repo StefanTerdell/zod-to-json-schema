50b1d2ec62038a27e8bfc78d23ab054463948c39
Stefan Terdell <stefan.terdell@live.com>
2021-09-30 09:25:41 +0200

    ✅ Added test for new number check

--- test/parsers/number.test.ts
+++ test/parsers/number.test.ts
@@ -34,6 +34,14 @@ describe('Number validations', () => {
     };
     expect(parsedSchema).toStrictEqual(jsonSchema);
   });
+  it('should be possible to describe multiples of n', () => {
+    const parsedSchema = parseNumberDef(z.number().multipleOf(2)._def);
+    const jsonSchema: JSONSchema7Type = {
+      type: 'number',
+      multipleOf: 2
+    };
+    expect(parsedSchema).toStrictEqual(jsonSchema);
+  });
   it('should be possible to describe positive, negative, nonpositive and nonnegative numbers', () => {
     const parsedSchema = parseNumberDef(
       z.number().positive().negative().nonpositive().nonnegative()._def
