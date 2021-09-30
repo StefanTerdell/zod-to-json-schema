2eb36a3c5f19aad56bedfd1805afe181236685c6
Stefan Terdell <stefan.terdell@live.com>
2021-09-30 09:49:29 +0200

    ✅ Add record tests

---
+++ test/parsers/record.test.ts
@@ -0,0 +1,59 @@
+import { z } from "zod";
+import { parseRecordDef } from "../../src/parsers/record";
+
+describe("records", () => {
+  it("should be possible to describe a simple record", () => {
+    const schema = z.record(z.number());
+
+    const parsedSchema = parseRecordDef(schema._def, [], []);
+    const expectedSchema = {
+      type: "object",
+      additionalProperties: {
+        type: "number",
+      },
+    };
+    expect(parsedSchema).toStrictEqual(expectedSchema);
+  });
+
+  it("should be possible to describe a complex record with checks", () => {
+    const schema = z.record(
+      z.object({ foo: z.number().min(2) }).catchall(z.string().cuid())
+    );
+
+    const parsedSchema = parseRecordDef(schema._def, [], []);
+    const expectedSchema = {
+      type: "object",
+      additionalProperties: {
+        type: "object",
+        properties: {
+          foo: {
+            type: "number",
+            minimum: 2,
+          },
+        },
+        required: ["foo"],
+        additionalProperties: {
+          type: "string",
+          pattern: "^c[^\\s-]{8,}$",
+        },
+      },
+    };
+    expect(parsedSchema).toStrictEqual(expectedSchema);
+  });
+
+  it("should be possible to describe a key schema", () => {
+    const schema = z.record(z.string().uuid(), z.number());
+
+    const parsedSchema = parseRecordDef(schema._def, [], []);
+    const expectedSchema = {
+      type: "object",
+      additionalProperties: {
+        type: "number",
+      },
+      propertyNames: {
+        format: "uuid",
+      },
+    };
+    expect(parsedSchema).toStrictEqual(expectedSchema);
+  });
+});
