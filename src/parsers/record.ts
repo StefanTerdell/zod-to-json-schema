dee4310ee087ce31475e8740120179cf0de327b0
Stefan Terdell <stefan.terdell@live.com>
2021-09-30 09:40:08 +0200

    🐛 Only allow strings as propertyNames in record

--- src/parsers/record.ts
+++ src/parsers/record.ts
@@ -1,28 +1,50 @@
-import { ZodRecordDef } from "zod";
+import { ZodFirstPartyTypeKind, ZodRecordDef, ZodTypeAny } from "zod";
 import { JsonSchema7Type, parseDef, Visited } from "../parseDef";
 import { JsonSchema7StringType, parseStringDef } from "./string";
+
+type JsonSchema7RecordPropertyNamesType = Omit<JsonSchema7StringType, "type">;
 
 export type JsonSchema7RecordType = {
   type: "object";
   additionalProperties: JsonSchema7Type;
-  propertyNames?: JsonSchema7StringType;
+  propertyNames?: JsonSchema7RecordPropertyNamesType;
 };
 
 export function parseRecordDef(
-  def: ZodRecordDef,
+  def: ZodRecordDef<ZodTypeAny, ZodTypeAny>,
   path: string[],
   visited: Visited
 ): JsonSchema7RecordType {
-  return {
-    type: "object",
-    additionalProperties:
-      parseDef(
-        def.valueType._def,
-        [...path, "additionalProperties"],
-        visited
-      ) || {},
-    propertyNames: def.keyType._def.checks?.length
-      ? parseStringDef(def.keyType._def)
-      : undefined,
-  };
+  if (
+    def.keyType._def.typeName === ZodFirstPartyTypeKind.ZodString &&
+    def.keyType._def.checks?.length
+  ) {
+    const keyType: JsonSchema7RecordPropertyNamesType = Object.entries(
+      parseStringDef(def.keyType._def)
+    ).reduce(
+      (acc, [key, value]) => (key === "type" ? acc : { ...acc, [key]: value }),
+      {}
+    );
+
+    return {
+      type: "object",
+      additionalProperties:
+        parseDef(
+          def.valueType._def,
+          [...path, "additionalProperties"],
+          visited
+        ) || {},
+      propertyNames: keyType,
+    };
+  } else {
+    return {
+      type: "object",
+      additionalProperties:
+        parseDef(
+          def.valueType._def,
+          [...path, "additionalProperties"],
+          visited
+        ) || {},
+    };
+  }
 }
