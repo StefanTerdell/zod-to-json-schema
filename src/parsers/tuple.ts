eb447ab722622793e363d9a764c65675735fc9a5
Stefan Terdell <stefan.terdell@live.com>
2021-09-30 09:52:10 +0200

    ✨ Added support for tuple.rest() and added tuple tests

--- src/parsers/tuple.ts
+++ src/parsers/tuple.ts
@@ -1,27 +1,55 @@
-import { ZodTupleDef } from "zod";
+import { ZodTupleDef, ZodTupleItems, ZodTypeAny } from "zod";
 import { JsonSchema7Type, parseDef, Visited } from "../parseDef";
 
 export type JsonSchema7TupleType = {
   type: "array";
   minItems: number;
-  maxItems: number;
   items: JsonSchema7Type[];
-};
+} & (
+  | {
+      maxItems: number;
+    }
+  | {
+      additionalItems?: JsonSchema7Type;
+    }
+);
 
 export function parseTupleDef(
-  def: ZodTupleDef,
+  def: ZodTupleDef<ZodTupleItems | [], ZodTypeAny | null>,
   path: string[],
   visited: Visited
 ): JsonSchema7TupleType {
-  return {
-    type: "array",
-    minItems: def.items.length,
-    maxItems: def.items.length,
-    items: def.items
-      .map((x, i) => parseDef(x._def, [...path, "items", i.toString()], visited))
-      .reduce(
-        (acc: JsonSchema7Type[], x) => (x === undefined ? acc : [...acc, x]),
-        []
+  if (def.rest) {
+    return {
+      type: "array",
+      minItems: def.items.length,
+      items: def.items
+        .map((x, i) =>
+          parseDef(x._def, [...path, "items", i.toString()], visited)
+        )
+        .reduce(
+          (acc: JsonSchema7Type[], x) => (x === undefined ? acc : [...acc, x]),
+          []
+        ),
+      additionalItems: parseDef(
+        def.rest._def,
+        [...path, "additionalItems"],
+        visited
       ),
-  };
+    };
+  } else {
+    return {
+      type: "array",
+      minItems: def.items.length,
+      maxItems: def.items.length,
+      items: def.items
+        .map((x, i) =>
+          parseDef(x._def, [...path, "items", i.toString()], visited)
+        )
+        .reduce(
+          (acc: JsonSchema7Type[], x) => (x === undefined ? acc : [...acc, x]),
+          []
+        ),
+    };
+  }
 }
---
+++ test/parsers/tuple.test.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+import { parseTupleDef } from "../../src/parsers/tuple";
+
+describe("objects", () => {
+  it("should be possible to describe a simple tuple schema", () => {
+    const schema = z.tuple([z.string(), z.number()]);
+
+    const parsedSchema = parseTupleDef(schema._def, [], []);
+    const expectedSchema = {
+      type: "array",
+      items: [{ type: "string" }, { type: "number" }],
+      minItems: 2,
+      maxItems: 2,
+    };
+    expect(parsedSchema).toStrictEqual(expectedSchema);
+  });
+
+  it("should be possible to describe a tuple schema with rest()", () => {
+    const schema = z.tuple([z.string(), z.number()]).rest(z.boolean());
+
+    const parsedSchema = parseTupleDef(schema._def, [], []);
+    const expectedSchema = {
+      type: "array",
+      items: [{ type: "string" }, { type: "number" }],
+      minItems: 2,
+      additionalItems: {
+        type: "boolean",
+      },
+    };
+    expect(parsedSchema).toStrictEqual(expectedSchema);
+  });
+});
