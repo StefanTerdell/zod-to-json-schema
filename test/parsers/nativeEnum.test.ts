e9b96dc0de360870121013d84dbfeb730fcf463e
mokocm <mokocmmoko@gmail.com>
2022-10-21 04:40:13 +0900

    Fixes to allow const assertions in nativeEnum

--- test/parsers/nativeEnum.test.ts
+++ test/parsers/nativeEnum.test.ts
@@ -47,4 +47,50 @@ describe('Native enums', () => {
     };
     expect(parsedSchema).toStrictEqual(jsonSchema);
   });
+
+  it('should be possible to convert a native const assertion object', () => {
+    const MyConstAssertionObject = {
+      val1: 0,
+      val2: 1,
+      val3: 2
+    } as const
+
+    const parsedSchema = parseNativeEnumDef(z.nativeEnum(MyConstAssertionObject)._def);
+    const jsonSchema: JSONSchema7Type = {
+      type: 'number',
+      enum: [0, 1, 2],
+    };
+    expect(parsedSchema).toStrictEqual(jsonSchema);
+  })
+
+  it('should be possible to convert a native const assertion string object', () => {
+    const MyConstAssertionObject = {
+      val1: 'a',
+      val2: 'b',
+      val3: 'c'
+    } as const
+
+    const parsedSchema = parseNativeEnumDef(z.nativeEnum(MyConstAssertionObject)._def);
+    const jsonSchema: JSONSchema7Type = {
+      type: 'string',
+      enum: ['a', 'b', 'c'],
+    };
+    expect(parsedSchema).toStrictEqual(jsonSchema);
+  })
+
+
+  it('should be possible to convert a mixed value native const assertion string object', () => {
+    const MyConstAssertionObject = {
+      val1: 'a',
+      val2: 1,
+      val3: 'c'
+    } as const
+
+    const parsedSchema = parseNativeEnumDef(z.nativeEnum(MyConstAssertionObject)._def);
+    const jsonSchema: JSONSchema7Type = {
+      type: ['string', 'number'],
+      enum: ['a', 1, 'c'],
+    };
+    expect(parsedSchema).toStrictEqual(jsonSchema);
+  })
 });
