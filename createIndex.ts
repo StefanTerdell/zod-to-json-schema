ea4d43520e8b14e3f5cc51890bb8505aa1b59a7a
Stefan Terdell <stefan.terdell@live.com>
2023-11-17 19:47:38 +0100

    🎇 Export everything from index

---
+++ createIndex.ts
@@ -0,0 +1,26 @@
+import { readdirSync, writeFileSync, statSync } from "fs"
+
+function checkSrcDir(path: string): string[] {
+  const lines: string[] = []
+
+  for (const item of readdirSync(path)) {
+    const itemPath = path + "/" + item
+
+    if (statSync(itemPath).isDirectory()) {
+      lines.push(...checkSrcDir(itemPath))
+    } else if (item.endsWith(".ts")) {
+      lines.push('export * from "./' + itemPath.slice(4, -2) + 'js"')
+    }
+  }
+
+  return lines
+}
+
+const lines = checkSrcDir("src")
+
+lines.push(
+  'import { zodToJsonSchema } from "./zodToJsonSchema.js"',
+  "export default zodToJsonSchema",
+)
+
+writeFileSync("./src/index.ts", lines.join("\n"))
