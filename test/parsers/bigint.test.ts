c052e90f0ed84b8c3011eedffa11c43eda7b6463
Brett Zamir <brettz9@yahoo.com>
2024-09-07 12:28:20 +0800

    ✅ bigints

--- test/parsers/bigint.test.ts
+++ test/parsers/bigint.test.ts
@@ -27,4 +27,65 @@ suite("bigint", (test) => {
     };
     assert(parsedSchema, jsonSchema);
   });
+
+  test("should be possible to define gt/lt (jsonSchema2019-09)", (assert) => {
+    const parsedSchema = parseBigintDef(
+      z.bigint().gte(BigInt(10)).lte(BigInt(20))._def,
+      getRefs({
+        target: 'jsonSchema2019-09'
+      }),
+    );
+    const jsonSchema = {
+      type: "integer",
+      format: "int64",
+      minimum: BigInt(10),
+      maximum: BigInt(20),
+    };
+    assert(parsedSchema, jsonSchema);
+  });
+
+  test("should be possible to define gt/lt", (assert) => {
+    const parsedSchema = parseBigintDef(
+      z.bigint().gt(BigInt(10)).lt(BigInt(20))._def,
+      getRefs(),
+    );
+    const jsonSchema = {
+      type: "integer",
+      format: "int64",
+      exclusiveMinimum: BigInt(10),
+      exclusiveMaximum: BigInt(20),
+    };
+    assert(parsedSchema, jsonSchema);
+  });
+
+  test("should be possible to define gt/lt (jsonSchema2019-09)", (assert) => {
+    const parsedSchema = parseBigintDef(
+      z.bigint().gt(BigInt(10)).lt(BigInt(20))._def,
+      getRefs({
+        target: "jsonSchema2019-09"
+      }),
+    );
+    const jsonSchema = {
+      type: "integer",
+      format: "int64",
+      exclusiveMinimum: true,
+      exclusiveMaximum: true,
+      minimum: BigInt(10),
+      maximum: BigInt(20)
+    };
+    assert(parsedSchema, jsonSchema);
+  });
+
+  test("should be possible to define multipleOf", (assert) => {
+    const parsedSchema = parseBigintDef(
+      z.bigint().multipleOf(BigInt(5))._def,
+      getRefs()
+    );
+    const jsonSchema = {
+      type: "integer",
+      format: "int64",
+      multipleOf: BigInt(5)
+    };
+    assert(parsedSchema, jsonSchema);
+  });
 });
