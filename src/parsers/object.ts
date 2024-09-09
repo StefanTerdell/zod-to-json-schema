ed733c26acc85d54971e219d2908a3b0dd1f3511
Stefan Terdell <stefan.terdell+git@proton.me>
2024-09-09 20:23:12 +0200

    🔥 Remove unused code

--- src/parsers/object.ts
+++ src/parsers/object.ts
@@ -6,25 +6,19 @@ function decideAdditionalProperties(def: ZodObjectDef, refs: Refs) {
   if (refs.removeAdditionalStrategy === "strict") {
     return def.catchall._def.typeName === "ZodNever"
       ? def.unknownKeys !== "strict"
-      : parseDef(
-        def.catchall._def,
-        {
+      : parseDef(def.catchall._def, {
           ...refs,
           currentPath: [...refs.currentPath, "additionalProperties"],
-        },
-      ) ?? true;
+        }) ?? true;
   } else {
     return def.catchall._def.typeName === "ZodNever"
       ? def.unknownKeys === "passthrough"
-      : parseDef(
-        def.catchall._def,
-        {
+      : parseDef(def.catchall._def, {
           ...refs,
           currentPath: [...refs.currentPath, "additionalProperties"],
-        },
-      ) ?? true;
+        }) ?? true;
   }
-};
+}
 
 export type JsonSchema7ObjectType = {
   type: "object";
@@ -32,76 +26,6 @@ export type JsonSchema7ObjectType = {
   additionalProperties: boolean | JsonSchema7Type;
   required?: string[];
 };
-
-export function parseObjectDefX(def: ZodObjectDef, refs: Refs) {
-  Object.keys(def.shape()).reduce(
-    (schema: JsonSchema7ObjectType, key) => {
-      let prop = def.shape()[key];
-
-      const isOptional = prop.isOptional();
-
-      if (!isOptional) {
-        prop = { ...prop._def.innerSchema };
-      }
-
-      const propSchema = parseDef(prop._def, {
-        ...refs,
-        currentPath: [...refs.currentPath, "properties", key],
-        propertyPath: [...refs.currentPath, "properties", key],
-      });
-
-      if (propSchema !== undefined) {
-        schema.properties[key] = propSchema;
-
-        if (!isOptional) {
-          if (!schema.required) {
-            schema.required = [];
-          }
-
-          schema.required.push(key);
-        }
-      }
-
-      return schema;
-    },
-    {
-      type: "object",
-      properties: {},
-      additionalProperties: decideAdditionalProperties(def, refs),
-    },
-  );
-
-  const result: JsonSchema7ObjectType = {
-    type: "object",
-    ...Object.entries(def.shape()).reduce(
-      (
-        acc: {
-          properties: Record<string, JsonSchema7Type>;
-          required: string[];
-        },
-        [propName, propDef],
-      ) => {
-        if (propDef === undefined || propDef._def === undefined) return acc;
-        const parsedDef = parseDef(propDef._def, {
-          ...refs,
-          currentPath: [...refs.currentPath, "properties", propName],
-          propertyPath: [...refs.currentPath, "properties", propName],
-        });
-        if (parsedDef === undefined) return acc;
-        return {
-          properties: { ...acc.properties, [propName]: parsedDef },
-          required: propDef.isOptional()
-            ? acc.required
-            : [...acc.required, propName],
-        };
-      },
-      { properties: {}, required: [] },
-    ),
-    additionalProperties: decideAdditionalProperties(def, refs),
-  };
-  if (!result.required!.length) delete result.required;
-  return result;
-}
 
 export function parseObjectDef(def: ZodObjectDef, refs: Refs) {
   const result: JsonSchema7ObjectType = {
